5417a4780e949163955515b1056636c5419069f5
kites262 <kites262@qq.com>
2025-07-21 22:13:06 +0800

    build: register oss

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/distribution/distribution/v3/registry/storage/driver/middleware/cloudfront"
 	_ "github.com/distribution/distribution/v3/registry/storage/driver/middleware/redirect"
 	_ "github.com/distribution/distribution/v3/registry/storage/driver/middleware/rewrite"
+	_ "github.com/distribution/distribution/v3/registry/storage/driver/oss"
 	_ "github.com/distribution/distribution/v3/registry/storage/driver/s3-aws"
 )
 
